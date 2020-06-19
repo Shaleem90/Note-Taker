8d70c2148e498ea2a60127543de2b20062bba120
Shaleem Malik <shaleemmalik6@gmail.com>
2020-06-19 18:53:47 -0400

    Made some changes to server.js routes

--- server.js
+++ server.js
@@ -16,90 +16,90 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //API routes
 app.get("/api/notes", (req, res) => {
-   
-    fs.readFile(__dirname + "/db/db.json", (err, data) => { 
-      if (err) throw err;
-      
-      let noteData = JSON.parse(data);
-      res.json(noteData);
-    })
-  });
-  // Post 
-  app.post("/api/notes", (req, res) => {
-  
-    fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => { 
-      if (err) throw err;
-      let notes = JSON.parse(data)
-    
-      let newNote = {
-        id: notes.length +1,
-        title: req.body.title,
-        text: req.body.text,
-      }
-    
-      notes.push(newNote)
-     
-      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err, data) => {
-        if (err) throw err;
-        res.json(notes)      
-      }); 
-    });
-  });
-  
-  // get a specific note to read
-  app.get("/api/notes/:title", (req, res) => {
- 
-    fs.readFile(__dirname + "/db/db.json", (err, data) => { 
-      if (err) throw err;
-      
-      let noteData = JSON.parse(data);
-      
-      notes = noteData.filter(data => {
-        return data.title.toLowerCase() === req.params.title.toLowerCase()
-      }); 
-      res.json(notes);
-    })
-  });
-  
-  app.delete("/api/notes/:id", (req, res) => {
-    
-    fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => { 
-      if (err) throw err;
-   
-      let notes = JSON.parse(data)
-    
-      const note = notes.find(n => n.id === parseInt(req.params.id));
-      
-      const index = notes.indexOf(note);
-      
-      notes.splice(index, 1);
-     
-      
-      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err, data) => {
-        if (err) throw err;
-        
-        res.json(notes)    
-      }); 
-    });
-  });
-  
-  //Routes
-  app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
-  
-  app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
-  
-  
-  app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/assets/css/styles.css"));
-  
 
-  app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/assets/js/index.js"));
-  
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
+        if (err) throw err;
+
+        let noteData = JSON.parse(data);
+        res.json(noteData);
+    })
+});
+// Post 
+app.post("/api/notes", (req, res) => {
+
+    fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data)
+
+        let newNote = {
+            id: notes.length + 1,
+            title: req.body.title,
+            text: req.body.text,
+        }
+
+        notes.push(newNote)
+
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err, data) => {
+            if (err) throw err;
+            res.json(notes)
+        });
+    });
+});
+
+// get a specific note to read
+app.get("/api/notes/:title", (req, res) => {
+
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
+        if (err) throw err;
+
+        let noteData = JSON.parse(data);
+
+        notes = noteData.filter(data => {
+            return data.title.toLowerCase() === req.params.title.toLowerCase()
+        });
+        res.json(notes);
+    })
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+
+    fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        let notes = JSON.parse(data)
+
+        const note = notes.find(n => n.id === parseInt(req.params.id));
+
+        const index = notes.indexOf(note);
+
+        notes.splice(index, 1);
+
+
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err, data) => {
+            if (err) throw err;
+
+            res.json(notes)
+        });
+    });
+});
+
+//Routes
+app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
+
+
+app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/assets/css/styles.css"));
+
+
+app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/assets/js/index.js"));
+
 
 
 
 
 
 // Port Listening on server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("SERVER IS LISTENING ON PORT: " + PORT);
-  });
+});
